265c54f58c97d2e81688d86a36aa9e8ee9249502
Nick <postbardforever@gmail.com>
2020-04-26 18:40:18 +0300

    Add Spring Conf. closes #21

--- pet-clinic-data/src/main/java/com/nick/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/nick/petclinic/services/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package com.nick.petclinic.services.map;
 
 import com.nick.petclinic.model.Owner;
 import com.nick.petclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
