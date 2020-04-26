6b24d5846c5fd1f8a9059c905b2c7fcfde58d7fb
Nick <postbardforever@gmail.com>
2020-04-26 16:36:53 +0300

    Impl Map based Services. closes #5

---
+++ pet-clinic-data/src/main/java/com/nick/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,40 @@
+package com.nick.petclinic.services.map;
+
+import com.nick.petclinic.model.Owner;
+import com.nick.petclinic.services.OwnerService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    @Override
+    @Deprecated
+    public Owner findByLastName(String lastName) {
+        return null;
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
