077bb74ae44ee1c5132227a589884d2c0374c38a
Nick <postbardforever@gmail.com>
2020-04-27 21:08:55 +0300

    Add Specialty, PetType - Service interfaces and Map impl. closes #28, #29, #30

---
+++ pet-clinic-data/src/main/java/com/nick/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.nick.petclinic.services;
+
+import com.nick.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/nick/petclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.nick.petclinic.services;
+
+import com.nick.petclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
