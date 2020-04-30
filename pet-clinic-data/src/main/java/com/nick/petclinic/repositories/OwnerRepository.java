b8bd85122e0c20602f784fe2a6168ac425589e8a
Nick <postbardforever@gmail.com>
2020-04-30 12:07:10 +0300

    Add OwnerService, that utilizes CrudRepository. closes #39

--- pet-clinic-data/src/main/java/com/nick/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/nick/petclinic/repositories/OwnerRepository.java
@@ -3,5 +3,10 @@ package com.nick.petclinic.repositories;
 import com.nick.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Optional<Owner> findByLastName(String lastName);
+
 }
