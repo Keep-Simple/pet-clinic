d24cb0e575e92c88ac5e65575220fd1c880040b2
Nick <postbardforever@gmail.com>
2020-04-30 12:15:42 +0300

    Add PetTypeService, that utilizes CrudRepository. closes #41

---
+++ pet-clinic-data/src/main/java/com/nick/petclinic/services/springdatajpa/PetTypeSDService.java
@@ -0,0 +1,44 @@
+package com.nick.petclinic.services.springdatajpa;
+
+import com.nick.petclinic.model.PetType;
+import com.nick.petclinic.repositories.PetTypeRepository;
+import com.nick.petclinic.services.PetTypeService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetTypeSDService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
